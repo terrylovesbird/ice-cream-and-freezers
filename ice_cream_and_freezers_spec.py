4369e8518daf6a3abc04e8bb74dbd124e4bdad42
Mic-J-Lee <mleenavy@gmail.com>
2019-09-17 10:44:55 -0500

    Update ice_cream_and_freezers_spec.py

--- ice_cream_and_freezers_spec.py
+++ ice_cream_and_freezers_spec.py
@@ -3,7 +3,7 @@ from ice_cream_and_freezers import IceCream, Freezer
 
 
 class TestIceCreamAndFreezers(unittest.TestCase):
-  # Go ahead and write some tests here for your code. We know that you'll at least need the IceCream and Freezer class.
+    # Go ahead and write some tests here for your code. We know that you'll at least need the IceCream and Freezer class.
 
 if __name__ == '__main__':
     unittest.main()
